dfaa4fbfff1f2ffb8e5402fc6ba784c6ca7b3f2c
KeNobeee <75133315+KeNobeee@users.noreply.github.com>
2024-04-01 19:20:57 +0400

    feat(label): added score and timer into UI

--- assets/scripts/GameManager.ts
+++ assets/scripts/GameManager.ts
@@ -1,10 +1,23 @@
-import { _decorator, Component, input, Input, EventMouse, screen, Node} from 'cc';
+import { _decorator, Component, input, Input, EventMouse, screen, Node, Label} from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameManager')
 export class GameManager extends Component {
     @property(Node)
     bucket: Node = null;
+
+    @property(Label)
+    timerLabel: Label;
+    @property(Label)
+    scoreLabel: Label;
+
+    timer:number = 19;
+    score:number = 0
+
+    endGame() {
+        this.unschedule(this.updateTimer);
+        console.log('end');
+    }
 
     onMouseMove(event: EventMouse) {
         const mousePositionX = event.getLocation().x;
@@ -15,8 +28,19 @@ export class GameManager extends Component {
         }
     }
 
+    updateTimer() {
+        if (this.timerLabel) {
+            this.timer -= 1;
+            this.timerLabel.string = `${this.timer}`;
+
+            if (this.timer <= 0) this.endGame();
+        }
+    }
+
     onLoad() {
         input.on(Input.EventType.MOUSE_MOVE, this.onMouseMove, this);
+
+        this.schedule(this.updateTimer, 1);
     }
 
     onDestroy() {
